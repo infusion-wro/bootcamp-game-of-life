64cf4c24806c3ae710bcd57adb5adddaea9d57a0
weeniearms <weeniearms@gmail.com>
2016-02-16 09:59:54 +0000

    Added solution

--- survive.js
+++ survive.js
@@ -1,5 +1,21 @@
 import States from './states';
 
 export default (state, neighbours) => {
+  if (state === States.LIVE && neighbours < 2) {
+    return States.DEAD;
+  }
+
+  if (state === States.LIVE && (neighbours == 2 || neighbours == 3)) {
+    return States.LIVE;
+  }
+
+  if (state === States.LIVE && neighbours > 3) {
+    return States.DEAD;
+  }
+
+  if (state == States.DEAD && neighbours == 3) {
+    return States.LIVE;
+  }
+
   return undefined;
 }
--- test/survive.test.js
+++ test/survive.test.js
@@ -3,4 +3,28 @@ import survive from '../survive';
 import States from '../states';
 
 describe('survive', function(){
+  it('should return DEAD for live cell with fewer than two neighbours', function(){
+    let state = survive(States.LIVE, 1);
+    expect(state).to.equal(States.DEAD);
+  });
+
+  it('should return LIVE for live cell with two neighbours', function(){
+    let state = survive(States.LIVE, 2);
+    expect(state).to.equal(States.LIVE);
+  });
+
+  it('should return LIVE for live cell with three neighbours', function(){
+    let state = survive(States.LIVE, 3);
+    expect(state).to.equal(States.LIVE);
+  });
+
+  it('should return DEAD for live cell with more than three neighbours', function(){
+    let state = survive(States.LIVE, 4);
+    expect(state).to.equal(States.DEAD);
+  });
+
+  it('should return LIVE for dead cell with three neighbours', function(){
+    let state = survive(States.DEAD, 3);
+    expect(state).to.equal(States.LIVE);
+  });
 });
